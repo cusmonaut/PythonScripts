a5394d65b2b3d048a4fea7f6c33509cd499fcd02
cusmonaut <vodtech@hotmail.com>
2014-12-06 12:57:26 -0600

    Fixed logic issue that broke checkin. Also made calculations and plate display more precise

--- plateCalc.py
+++ plateCalc.py
@@ -7,12 +7,11 @@ import math
 
 kilogram = 2.20462262185
 largestPlate = 20 #40 if the gym has it
+barbell = 45 / kilogram
 
 def getKg( weight ):	
 	try:
 		convertedWeight = float(weight)
-		#remove bar
-		convertedWeight -= 45
 		convertedWeight = convertedWeight / kilogram
 	except:
 		print("Enter a number dickweed.")
@@ -20,8 +19,9 @@ def getKg( weight ):
 	return convertedWeight
 
 def getSide( weight ):
-	singleSide = weight / 2.00
-	return(math.floor(singleSide))
+		singleSide = float(weight) / 2.00
+		singleSide -= (barbell / 2.00)
+		return(singleSide)
 
 #Buckle up, it's recursion time
 def getPlates( weight, plate ):
@@ -31,20 +31,23 @@ def getPlates( weight, plate ):
 	if not(plateCount < 1):
 		print("%0.1Fkg x %d" % (plate, plateCount))
 
-	if(remaningWeight < 2.5):
+	if(remaningWeight <= 0 or plate == 0.5):
 		return
 
-	if not(plate <= 2.5):
+	if (plate < 5):
+		getPlates(remaningWeight, (plate - 0.5))
+	else:
 		getPlates(remaningWeight, plate/2)
 
 tableLines = "\n-------------------------\n"
 
 while(True):
 	user_input = input("Enter Weight in LBS: ")
-	side = getSide(weight)
+	kgWeight = getKg(user_input)
+	side = getSide(kgWeight)
 	print(tableLines)
 	print("Weights assume a 45lb bar")
-	print("Weight per side: %dkg" % (side))
+	print("Weight per side: %0.1Fkg" % (side))
 	print("-----Plates per side-----")
 	getPlates(side,largestPlate)
 	print(tableLines)
